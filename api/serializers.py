2a35c4c134dc699f1ae675d41369c53729da60bc
EnzoRobaina <enzo.robaina@gmail.com>
2019-11-20 22:02:23 -0300

    Adiciona validação aos atributos numéricos de Character

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,37 @@ from rest_framework import serializers
 from .models import Character, Skill
 
 
+def clean_numerical_field(value):
+    if value <= 0 or value > 20:
+        raise serializers.ValidationError(
+            'This value must be between 1 and 20.',
+            code='invalid'
+        )
+    return value
+
+
 class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ('__all__')
+
+    def validate_strength(self, value):
+        return clean_numerical_field(value)
+
+    def validate_dexterity(self, value):
+        return clean_numerical_field(value)
+
+    def validate_constitution(self, value):
+        return clean_numerical_field(value)
+
+    def validate_intelligence(self, value):
+        return clean_numerical_field(value)
+
+    def validate_wisdom(self, value):
+        return clean_numerical_field(value)
+
+    def validate_charisma(self, value):
+        return clean_numerical_field(value)
 
 
 class SkillSerializer(serializers.ModelSerializer):
