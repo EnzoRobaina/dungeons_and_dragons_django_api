bd6b323fc283ce709dd9832b02f84e547c9dd04c
EnzoRobaina <enzo.robaina@gmail.com>
2019-11-20 21:53:30 -0300

    Registra urls de character e skill

--- trabalho/urls.py
+++ trabalho/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+
+router.register(r'characters', views.CharacterViewSet)
+router.register(r'skills', views.SkillViewSet)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
