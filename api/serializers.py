077e26f4ad4a95e75ac6fdca0a415fb2548802fd
EnzoRobaina <enzo.robaina@gmail.com>
2019-11-26 21:03:40 -0300

    Adiciona score à Skill

--- api/serializers.py
+++ api/serializers.py
@@ -11,14 +11,20 @@ def clean_numerical_field(value):
     return value
 
 
+class CharacterSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = ('__all__')
+        read_only_fields = ['level', 'proficiency_bonus']
+
+
 class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ('__all__')
-        read_only_fields = ['level']
+        read_only_fields = ['level', 'proficiency_bonus']
 
     skills = serializers.SerializerMethodField()
-    proficiency_bonus = serializers.SerializerMethodField()
 
     def get_skills(self, object):
         skills = SkillSerializer(
@@ -28,19 +34,6 @@ class CharacterSerializer(serializers.ModelSerializer):
         for skill in skills:
             skill.pop('character')
         return skills
-
-    def get_proficiency_bonus(self, object):
-        level = object.level
-        if level >= 1 and level <= 4:
-            return 2
-        if level >= 5 and level <= 8:
-            return 3
-        if level >= 9 and level <= 12:
-            return 4
-        if level >= 13 and level <= 16:
-            return 5
-        if level >= 17 and level <= 20:
-            return 6
 
     def validate_strength(self, value):
         return clean_numerical_field(value)
@@ -65,3 +58,29 @@ class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
         fields = ('__all__')
+
+    score = serializers.SerializerMethodField()
+
+    def _get_modifier_list(self):
+        return {
+            1: -5,
+            **dict.fromkeys([2, 3], -4),
+            **dict.fromkeys([4, 5], -3),
+            **dict.fromkeys([6, 7], -2),
+            **dict.fromkeys([8, 9], -41),
+            **dict.fromkeys([10, 11], 0),
+            **dict.fromkeys([12, 13], 1),
+            **dict.fromkeys([14, 15], 2),
+            **dict.fromkeys([16, 17], 3),
+            **dict.fromkeys([18, 19], 4),
+            20: 5
+        }
+
+    def get_score(self, object):
+        character = CharacterSimpleSerializer(
+            instance=object.character
+        ).data
+        score = self._get_modifier_list()[character[object.ability]]
+        if object.proficient:
+            score += character['proficiency_bonus']
+        return score
