bcff955b5ff244a30352afbfb089798c723f10c2
EnzoRobaina <enzo.robaina@gmail.com>
2019-11-27 19:23:20 -0300

    Adiciona testes de controller p/ Skill

--- api/tests.py
+++ api/tests.py
@@ -20,7 +20,15 @@ def create_character(character):
         data=character
     )
     serializer.is_valid(raise_exception=True)
-    serializer.save()
+    return serializer.save()
+
+
+def create_skill(skill):
+    serializer = SkillSerializer(
+        data=skill
+    )
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
 
 
 class CharacterControllerTests(APITestCase):
@@ -51,6 +59,65 @@ class CharacterControllerTests(APITestCase):
             reverse(
                 'character-detail',
                 kwargs={'pk': 1}
+            ),
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class SkillControllerTests(APITestCase):
+    def setUp(self):
+        self.character = create_character(CHARACTERS['elisson'])
+        self.skill = create_skill(
+            {
+                **SKILLS['history'],
+                "character": self.character.id
+            }
+        )
+
+    def test_should_create_skill(self):
+        skill_data = SKILLS['athletics']
+        skill_data.pop('character')
+        response = self.client.post(
+            reverse(
+                'skills_manager',
+                kwargs={
+                    'character_pk': self.character.id,
+                }
+            ),
+            data=skill_data,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Skill.objects.count(), 2)
+        self.assertEqual(Skill.objects.get(pk=2).name, SKILLS['athletics']['name'])
+
+    def test_should_update_skill(self):
+        response = self.client.patch(
+            reverse(
+                'skills_nested_in_character',
+                kwargs={
+                    'character_pk': self.character.id,
+                    'skill_pk': self.skill.id
+                }
+            ),
+            data={
+                "name": SKILLS['athletics']['name'],
+                "ability": SKILLS['athletics']['ability']
+            },
+            format='json'
+        )
+        self.assertNotEqual(Skill.objects.get().name, self.skill.name)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_should_destroy_skill(self):
+        response = self.client.delete(
+            reverse(
+                'skills_nested_in_character',
+                kwargs={
+                    'character_pk': self.character.id,
+                    'skill_pk': self.skill.id
+                }
             ),
             format='json'
         )
